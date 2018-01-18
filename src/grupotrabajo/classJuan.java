cc8b50481ce589b4a93917f53251e15703880d30
juan <juan@EQUIP1.equipo>
2018-01-18 20:04:07 +0100

    ya se han creado los botones y funciona

--- src/grupotrabajo/classJuan.java
+++ src/grupotrabajo/classJuan.java
@@ -18,7 +18,7 @@ public class classJuan extends javax.swing.JFrame {
      */
     public classJuan() {
         initComponents();
-        this.getContentPane().setBackground(Color.green);
+        this.getContentPane().setBackground(Color.blue);
     }
 
     /**
